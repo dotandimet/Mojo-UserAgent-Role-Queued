cc18ed55ef34ab7fc9ce7893e7ab3ca51184bedf
Dotan Dimet <dotan@corky.net>
2017-12-26 17:19:46 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Mojo::UserAgent::Role::Queued - It's new $module
+
+# SYNOPSIS
+
+    use Mojo::UserAgent::Role::Queued;
+
+# DESCRIPTION
+
+Mojo::UserAgent::Role::Queued is ...
+
+# LICENSE
+
+Copyright (C) Dotan Dimet.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Dotan Dimet <dotan@corky.net>
---
+++ lib/Mojo/UserAgent/Role/Queued.pm
@@ -0,0 +1,39 @@
+package Mojo::UserAgent::Role::Queued;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Mojo::UserAgent::Role::Queued - It's new $module
+
+=head1 SYNOPSIS
+
+    use Mojo::UserAgent::Role::Queued;
+
+=head1 DESCRIPTION
+
+Mojo::UserAgent::Role::Queued is ...
+
+=head1 LICENSE
+
+Copyright (C) Dotan Dimet.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Dotan Dimet E<lt>dotan@corky.netE<gt>
+
+=cut
+
