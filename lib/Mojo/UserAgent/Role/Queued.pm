caac8b178596d7c08852387ecf9e4128c439e392
Dotan Dimet <dotan@corky.net>
2018-01-01 16:08:25 +0200

    Checking in changes prior to tagging of version 0.03.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index e67add0..9aa00a9 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,8 @@ Revision history for Perl extension Mojo-UserAgent-Role-Queued
    
     {{$NEXT}}
    
    +0.03 2018-01-01T14:07:28Z
    +
         - added missing Class::Method::Modifiers dependency
         - fix bug where more than one timer was created

--- lib/Mojo/UserAgent/Role/Queued.pm
+++ lib/Mojo/UserAgent/Role/Queued.pm
@@ -2,7 +2,7 @@ package Mojo::UserAgent::Role::Queued;
 use Mojo::Base '-role';
 use Scalar::Util 'weaken';
 
-our $VERSION = "0.02";
+our $VERSION = "0.03";
 
 has max_active => sub { shift->max_connections };
 has active         => 0;
